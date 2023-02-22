85890148e1aa117093842506c2459758648a8286
endurancecoding <endurancegodfrey10@gmail.com>
2023-02-21 23:52:21 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,3 +1,5 @@
+
+
 #include "main.h"
 
 /**
@@ -11,7 +13,6 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 	{
 	for (num = 0; num <= n; num++)
-	
 	{
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
@@ -35,7 +36,6 @@ void print_times_table(int n)
 	_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
-
 	}
 	}
 }
