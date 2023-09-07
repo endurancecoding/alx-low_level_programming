7660f47684d09f4879712c4b8d6b10e065680b9c
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 <endurancecoding@gmail.com>
2023-09-07 06:00:46 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: Always 1 (success)
+ * Return:1
  */
-int man(void)
+int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
